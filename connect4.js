aa5664fa7bddf8e8f664b5e70e694338d3b98045
berolso <insta11@hotmail.com>
2020-05-20 07:42:23 -0700

    refactor during code review

--- connect4.js
+++ connect4.js
@@ -2,7 +2,7 @@
  *
  * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
  * column until a player gets four-in-a-row (horiz, vert, or diag) or until
- * board fills (tie)
+ * board fills (tie)git 
  */
 
 let WIDTH = 7;
